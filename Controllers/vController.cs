bc0824ce2b33e5bfd57655c1c00fc312da5ba3bb
Jerron <jerron.byu@gmail.com>
2022-01-19 21:15:01 -0700

    Add project files.

---
+++ Controllers/vController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MissionThree.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MissionThree.Controllers
+{
+    public class vController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult gradeCal()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult gradeCal(gradeCalModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/gradeCalModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MissionThree.Models
+{
+    public class gradeCalModel
+    {
+        [Required]
+        [Range(0,100)]
+        public float assignments { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float groupProjects { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float quizzes { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float exams { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public float intex { get; set; }
+    }
+}
